1a3661b19eeab86ae8391acdf4c954d835597dcb
Oleksii Khomiakov <alex.xag1987@gmail.com>
2023-06-21 00:22:02 +0300

    Notes was studied

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+// import { App } from 'components/App';
+import { Provider } from 'react-redux';
+import { store } from 'study/redux/store';
 import './index.css';
+import { AppStudy } from 'study/componentsStudy/AppStudy';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      {/* <App /> */}
+      <AppStudy />
+    </Provider>
   </React.StrictMode>
 );
